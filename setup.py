a246c381a64be87c6e734cc01e61671cdebe213f
sachin <sachinsom507@gmail.com>
2021-06-05 13:50:52 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,63 @@
+"""
+Installable scripts for app application.
+"""
+from pathlib import Path
+from setuptools import setup, find_packages
+
+DESCRIPTION = (
+    "Boilerplate Flask API with Flask-SQLAlchemy, RESTx, pytest, flake8 "
+    "tox configured"
+)
+APP_ROOT = Path(__file__).parent
+README = (APP_ROOT / "README.md").read_text()
+AUTHOR = "sachin som"
+AUTHOR_EMAIL = "sachinsom507@gmail.com"
+PROJECT_URLS = {"Source Code": "https://github.com/sachinsom93/flask-boilerplate"}
+INSTALL_REQUIRES = [
+    "Flask",
+    "Flask-Bcrypt",
+    "Flask-Cors",
+    "Flask-Migrate",
+    "flask-restx",
+    "Flask-SQLAlchemy",
+    "PyJWT",
+    "python-dateutil",
+    "python-dotenv",
+    "requests",
+    "urllib3",
+    "werkzeug==0.16.1",
+]
+EXTRA_REQUIRE = {
+    "dev": [
+        "black",
+        "flake8",
+        "pre-commit",
+        "pydocstyle",
+        "pytest",
+        "pytest-black",
+        "pytest-clarity",
+        "pytest-dotenv",
+        "pytest-flake8",
+        "pytest-flask",
+        "tox",
+    ]
+}
+
+setup(
+    name="app",
+    description=DESCRIPTION,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    version="0.1",
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    maintainer_email=AUTHOR_EMAIL,
+    license="MIT",
+    url="https://github.com/sachinsom93/flask-boilerplate",
+    project_urls=PROJECT_URLS,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.6",
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRA_REQUIRE,
+)
